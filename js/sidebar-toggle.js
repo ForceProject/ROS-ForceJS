267396ee34d09d9e4902854898f115d87f5029f3
Lewis, Jordan <38559@joeys.org>
2016-02-16 15:22:47 +1100

    Fix the Toggle Button
    Toggle button now comes in and out as needed on smaller screens
    On larger screens, it is hidden.

--- js/sidebar-toggle.js
+++ js/sidebar-toggle.js
@@ -1,13 +1,14 @@
 $("#menu-toggle").click(function(e) {
-	e.preventDefault();
-	$("#wrapper").toggleClass("toggled");
-	$("#sidebar-wrapper").css({
-			top: $('header').height(),
-	});
-	$("#sidebar-wrapper").css({
-			"-webkit-transition": "all 0.5s ease",
-			"-moz-transition": "all 0.5s ease",
-			"-o-transition": "all 0.5s ease",
-			"transition": "all 0.5s ease"
-	});
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+    $("#menu-toggle").toggleClass("toggled");
+    $("#sidebar-wrapper").css({
+            top: $('header').height(),
+    });
+    $("#sidebar-wrapper").css({
+            "-webkit-transition": "all 0.5s ease",
+            "-moz-transition": "all 0.5s ease",
+            "-o-transition": "all 0.5s ease",
+            "transition": "all 0.5s ease"
+    });
 });
