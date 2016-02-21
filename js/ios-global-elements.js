d6256e575b0661263cc2959a186f82724da7368f
Jordan Lewis <jordan_j_lewis@icloud.com>
2016-02-21 22:23:29 +1100

    Begin adding iOS Docs
    iOS Docs, side bar now limits the height of the tiles section to 50vh
    Create and complete Button, Label, Horizontal and Vertical Sliders
    Begin working on Myo Tile Docs
    Add syntax highlighter to the iso-global-elements.js
    create start-syntaxhighlighter.js for starting the syntax highlighter

--- js/ios-global-elements.js
+++ js/ios-global-elements.js
@@ -6,7 +6,10 @@ jQuery(function ($) {
     <link href="../../../css/simple-sidebar.css" rel="stylesheet">\
     <!-- Accordion -->\
     <link href="../../../css/accordion.css" rel="stylesheet">\
-            ');
+    <!-- Syntax Highlighter -->\
+    <link href="../../../css/SyntaxHighlighter_3-0-83.0/shCore.css" rel="stylesheet">\
+    <link href="../../../css/SyntaxHighlighter_3-0-83.0/shThemeDefault.css" rel="stylesheet">\
+    ');
 
     head.add(links).appendTo(head);
 
@@ -21,75 +24,20 @@ jQuery(function ($) {
     appendScript("../../../js/sidebar-stick.js");
     appendScript("../../../js/sidebar-toggle.js");
     appendScript("../../../js/accordion.js");
+    appendScript("../../../js/SyntaxHighlighter_3-0-83.0/shCore.js");
+    appendScript("../../../js/SyntaxHighlighter_3-0-83.0/shBrushPython.js");
+    appendScript("../../../js/start-syntaxhighlighter.js");
 
     var body = $('#sidebar-wrapper');
-    var sideNav = $('<ul class="sidebar-nav">\
-                    <li class="sidebar-brand">\
-                        <a href="#">\
-                        iOS Client\
-                        </a>\
-                    </li>\
-                    <li>\
-                        <a href="Overview.html">Overview</a>\
-                    </li>\
-                    <li>\
-                        <a href="Template.html">Getting Started</a>\
-                    </li>\
-                     <li>\
-                        <ul class="accordion">\
-                            <li>\
-                                <div class="acc-header">\
-                                    <a href="#" class="head" title="Click to expand.">Tile Documentation</a>\
-                                    <a href="#" class="arrow">↓</a>\
-                                </div>\
-                                <div class="content">\
-                                    <ul>\
-                                        <li>\
-                                            <a href="Delegate_Functions.html">Button</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Built-In_Functions.html">Textfield</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Chat_Demo.html">Label</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Chat_Demo.html">Joystick</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Chat_Demo.html">Text View</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Chat_Demo.html">Image View</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Chat_Demo.html">MJPEG Stream View</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Chat_Demo.html">Switch</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Chat_Demo.html">Segmented Control</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Chat_Demo.html">Horizontal Slider</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Chat_Demo.html">Vertical Slider</a>\
-                                        </li>\
-                                        <li>\
-                                            <a href="Chat_Demo.html">Progress Bar</a>\
-                                        </li>\
-                                    </ul>\
-                                </div>\
-                            </li>\
-                        </ul>\
-                         <p><br/></p>\
-                    </li>\
-                    <li>\
-                        <a href="Known_Issues.html">Known Issues</a>\
-                    </li>\
-                </ul>');
+    //var sideNav = $('');
 
-    body.add(sideNav).appendTo(body);
+    $.ajax({
+            url : "sidenav.html",
+            dataType: "text",
+            success : function (data) {
+                body.add(data).appendTo(body);
+            }
+        });
+
+   // body.add(sideNav).appendTo(body);
 });
