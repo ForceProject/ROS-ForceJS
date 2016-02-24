58d0bc4dd75d1017c6a2af81ab5411e97a0ab943
Lewis, Jordan <38559@joeys.org>
2016-02-24 17:01:10 +1100

    Finish Myo tile doc

--- js/showPython.js
+++ js/showPython.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         // Declare Variables
         var datatypeSection = $(this).parent();
         var colContainer = datatypeSection.parent();
+        var dataStructure = datatypeSection.find('.dataStructure');
         var codeDiv = datatypeSection.find('.hidden-code');
 
         // Open and Close
@@ -15,13 +16,22 @@ $(document).ready(function () {
         var colContainerClassesArr = colContainerClasses.split(' ');
 
         if (colContainerClassesArr[2] == 'col-lg-12') {
+            // HIDE THE CODE
+
             var colContainerIds = colContainer.attr('id');
             colContainer.attr('class', colContainerIds);
             colContainer.attr('id', '');
 
+            dataStructure.removeClass('col-lg-4');
+            codeDiv.removeClass('col-lg-8');
+
+            codeDiv.find('h3').remove();
+
             // Set the Button to Show
             $(this).text('Show Python');
         } else {
+            // SHOW THE CODE
+
             // Set the id of the div to the classes for later reverting back
             colContainer.attr('id', colContainerClasses);
 
@@ -33,6 +43,12 @@ $(document).ready(function () {
             // Set the expanded
             colContainer.addClass('col-md-12');
             colContainer.addClass('col-lg-12');
+
+            // Some more
+            dataStructure.addClass('col-lg-4');
+            codeDiv.addClass('col-lg-8');
+
+            codeDiv.prepend('<h3>Usage Demo</h3>')
 
             // Set the Button to Hide
             $(this).text('Hide Python');
