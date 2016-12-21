78d1df076fb1c795855ac15e39a712258419738e
Jordan Joseph Lewis <jordan_j_lewis@me.com>
2016-12-21 15:29:39 +1100

    Remove all of the unused stuff from website

--- js/misc/randYouProfilePic.js
+++
@@ -1,7 +0,0 @@
-jQuery(function ($) {
-    var profile = $('#you-profile');
-    var rand = Math.floor((Math.random() * 23) + 1);
-    var imageName = rand + ".png";
-    var imagePath = "/img/team/you/profile/" + imageName;
-    profile.attr("src", imagePath);
-});
